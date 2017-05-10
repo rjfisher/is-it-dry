697b139e56842a1eae201c26d38b9a9afc60b33d
Rob Fisher <rfisher@sevone.com>
2017-05-10 15:11:02 -0400

    Getting to the deployment step in

--- gulpfile.js
+++ gulpfile.js
@@ -79,6 +79,11 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
+gulp.task('deploy', ['default'], () => {
+  return gulp.src('dist/**/*')
+    .pipe($.ghPages());
+});
+
 gulp.task('serve', () => {
   runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
     browserSync.init({
